0f6966a7493793f34fb76db13b252ac2bc99a015
Shaoxiong Yuan <syuan697@usc.edu>
2024-01-30 21:52:51 -0800

    project set up

---
+++ Dough to Bread/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dough to Bread
+//
+//  Created by Shaoxiong Yuan on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dough to Bread/Dough_to_BreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dough_to_BreadApp.swift
+//  Dough to Bread
+//
+//  Created by Shaoxiong Yuan on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Dough_to_BreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

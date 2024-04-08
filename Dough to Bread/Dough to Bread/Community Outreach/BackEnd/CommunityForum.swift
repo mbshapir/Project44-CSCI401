eaec120bca4cb7a75a161fb619f4081e85c3da1c
mbshapir <mbshapir@usc.edu>
2024-04-08 14:11:33 -0700

    Finished updating Community Forum reply feature

--- Dough to Bread/Dough to Bread/Community Outreach/BackEnd/CommunityForum.swift
+++ Dough to Bread/Dough to Bread/Community Outreach/BackEnd/CommunityForum.swift
@@ -10,8 +10,12 @@ import Firebase
 
 class ForumViewModel: ObservableObject {
     @Published var messages = [Message]()
+    @Published var errorMessage: String? //track error message state
     
     private var dbRef: DatabaseReference
+    
+    let badWords: Set<String> = ["fuck", "shit", "bitch", "kys", "damn", "bullshit", "whore", "wanker", "motherfuck", "motherfucker", "dickhead"] // library of bad words
+
     
     init() {
         self.dbRef = Database.database().reference(withPath: "communityMessages")
@@ -21,24 +25,56 @@ class ForumViewModel: ObservableObject {
     func fetchMessages() {
         dbRef.queryOrdered(byChild: "timestamp").observe(.value, with: { snapshot in
             var newMessages: [Message] = []
+            let group = DispatchGroup() // Use a dispatch group to handle asynchronous fetching of replies
+
             for child in snapshot.children {
                 if let snapshot = child as? DataSnapshot,
                    let value = snapshot.value as? [String: AnyObject],
                    let text = value["text"] as? String,
                    let timestamp = value["timestamp"] as? Double {
                     let id = snapshot.key
-                    let message = Message(id: id, text: text, timestamp: Date(timeIntervalSince1970: timestamp))
+                    var message = Message(id: id, text: text, timestamp: Date(timeIntervalSince1970: timestamp))
+
+                    // Check if the message has replies
+                    if let repliesSnapshot = value["replies"] as? [String: AnyObject] {
+                        group.enter() // Enter the group before fetching replies
+                        var replies: [Message] = []
+                        for (replyId, replyValue) in repliesSnapshot {
+                            if let replyDict = replyValue as? [String: AnyObject],
+                               let replyText = replyDict["text"] as? String,
+                               let replyTimestamp = replyDict["timestamp"] as? Double {
+                                let reply = Message(id: replyId, text: replyText, timestamp: Date(timeIntervalSince1970: replyTimestamp))
+                                replies.append(reply)
+                            }
+                        }
+                        message.replies = replies
+                        group.leave() // Leave the group after fetching replies
+                    }
+
                     newMessages.append(message)
                 }
             }
-            self.messages = newMessages.reversed()
+            
+            group.notify(queue: .main) {
+                self.messages = newMessages.reversed() // Update messages once all replies are fetched
+            }
         })
+    }
+
+    func containsBadWords(_ text: String) -> Bool {
+        let words = text.lowercased().split(separator: " ").map(String.init)
+        return words.contains(where: { badWords.contains($0) })
     }
     
     func sendMessage(_ messageText: String) {
         // Check if the message text is empty
         guard !messageText.isEmpty else {
-            print("Error: Message text is empty")
+            self.errorMessage = "Error: Message text is empty"
+            return
+        }
+        
+        guard !containsBadWords(messageText) else{
+            self.errorMessage = "Error: Cannot Upload Vulgar Message"
             return
         }
         
@@ -49,11 +85,34 @@ class ForumViewModel: ObservableObject {
         ]
         newMessageRef.setValue(newMessageData)
     }
+    
+    
+    func sendReply(to messageId: String, replyText: String) {
+        guard !replyText.isEmpty else {
+            self.errorMessage = "Error: Reply text is empty"
+            return
+        }
+        
+        guard !containsBadWords(replyText) else{
+            self.errorMessage = "Error: Cannot Upload Vulgar Reply"
+            return
+        }
+        
+        let replyRef = dbRef.child(messageId).child("replies").childByAutoId()
+        let replyData: [String: Any] = [
+            "text": replyText,
+            "timestamp": Date().timeIntervalSince1970
+        ]
+        replyRef.setValue(replyData)
+    }
 }
+
 
 struct Message: Identifiable {
     var id: String
     var text: String
     var timestamp: Date
+    var replies: [Message] = [] //same structure as og messages for replies
 }
+
 
--- Dough to Bread/Dough to Bread/Community Outreach/FrontEnd/CommunityForumView.swift
+++ Dough to Bread/Dough to Bread/Community Outreach/FrontEnd/CommunityForumView.swift
@@ -11,7 +11,11 @@ import Firebase
 struct CommunityForumView: View {
     @ObservedObject var viewModel = ForumViewModel()
     @State private var newMessageText = ""
+    @State private var replyTexts: [String: String] = [:] // To store replies text for each message
+    @State private var expandedMessageID: String? // To track which message's replies are shown
+    
     @Environment(\.presentationMode) var presentationMode
+    @State private var showAlert = false
 
     var body: some View {
         VStack(spacing: 0) {
@@ -45,15 +49,23 @@ struct CommunityForumView: View {
             .padding(.bottom)
 
             VStack {
-                List(viewModel.messages) { message in
-                    VStack(alignment: .leading) {
-                        Text(message.text)
-                            .padding()
-                        Text(message.timestamp, style: .time)
-                            .font(.caption)
-                            .foregroundColor(.gray)
+                List {
+                    ForEach(viewModel.messages, id: \.id) { message in
+                        MessageView(message: message,
+                                    replyText: Binding<String>(
+                                        get: { self.replyTexts[message.id] ?? "" },
+                                        set: { self.replyTexts[message.id] = $0 }
+                                    ),
+                                    expandedMessageID: $expandedMessageID,
+                                    sendMessage: viewModel.sendMessage,
+                                    sendReply: { replyText in
+                                        viewModel.sendReply(to: message.id, replyText: replyText)
+                                        self.replyTexts[message.id] = "" // Clear reply text field after sending
+                                    })
                     }
+
                 }
+                .listStyle(PlainListStyle()) // Optional: Removes list row separators
                 
                 HStack {
                     TextField("Enter your message here", text: $newMessageText)
@@ -90,8 +102,85 @@ struct CommunityForumView: View {
                 self.viewModel.fetchMessages()
             }
         }
+        .alert("Error", isPresented: $showAlert, presenting: viewModel.errorMessage) { errorMessage in
+            Button("OK") {
+                // Action when OK is pressed
+                viewModel.errorMessage = nil // Clear error message after showing alert
+            }
+        } message: { errorMessage in
+            Text(errorMessage)
+        }
+        .onChange(of: viewModel.errorMessage) {
+            showAlert = viewModel.errorMessage != nil
+        }
         .navigationBarTitle("", displayMode: .inline)
         .navigationBarHidden(true)
+    }
+}
+
+struct MessageView: View {
+    var message: Message
+    @Binding var replyText: String
+    @Binding var expandedMessageID: String?
+    var sendMessage: (String) -> Void
+    var sendReply: (String) -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(message.text)
+                .padding()
+                .font(.custom("Poppins-SemiBold", size: 20))
+            Text("\(message.timestamp, style: .date) @ \(message.timestamp, style: .time)")
+                .font(.custom("Poppins-SemiBold", size: 13))
+                .foregroundColor(.gray)
+            Button(action: {
+                // Toggle the visibility of the replies
+                expandedMessageID = (expandedMessageID == message.id) ? nil : message.id
+            }) {
+                Text("Reply (\(message.replies.count))")
+                    .foregroundColor(.blue)
+                    .font(.custom("Poppins-SemiBold", size: 13))
+                    .padding(3)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color(hex: "000000"), lineWidth: 1.25)
+                    )
+            }
+            .buttonStyle(PlainButtonStyle())
+            // Display replies if this message's replies are expanded
+            if expandedMessageID == message.id {
+                TextField("Enter your reply here", text: $replyText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.custom("Poppins-SemiBold", size: 18))
+                    .padding()
+                Button("Send Reply") {
+                    sendReply(replyText)
+                }
+                .foregroundColor(.blue)
+                .font(.custom("Poppins-SemiBold", size: 13))
+                .padding(3)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color(hex: "000000"), lineWidth: 2)
+                )
+                .buttonStyle(PlainButtonStyle())
+                
+                ForEach(message.replies.sorted(by: { $0.timestamp > $1.timestamp }), id: \.id) { reply in
+                    VStack(alignment: .leading) {
+                        Text(reply.text)
+                            .font(.custom("Poppins-SemiBold", size: 16))
+                            .padding([.leading, .top])
+                            .offset(x: 20, y: 0)
+                        Text("\(reply.timestamp, style: .date) @ \(reply.timestamp, style: .time)")
+                            .font(.custom("Poppins-SemiBold", size: 12))
+                            .foregroundColor(.gray)
+                            .offset(x: 37, y: 0)
+                    }
+                }
+            }
+        }
     }
 }
 

80a03f1a19f9f8f502b9a824a3554708d1e6abf1
Shaoxiong Yuan <syuan697@usc.edu>
2024-05-01 14:17:50 -0700

    original

---
+++ Dough to Bread/Dough to Bread/FinancialLiteracyModule.swift
@@ -0,0 +1,28 @@
+//
+//  FinancialLiteracyModule.swift
+//  Dough to Bread
+//
+//  Created by Shaoxiong Yuan on 2/7/24.
+//
+
+import Foundation
+
+class FinancialLiteracyModule: ObservableObject {
+    // Add any properties you need to manage the state
+
+    func goBack() {
+        // Code to handle the back navigation
+    }
+
+    func interactiveCourses() {
+        // Code to handle interactive courses
+    }
+
+    func quizzes() {
+        // Code to handle quizzes
+    }
+
+    func progressTracking() {
+        // Code to handle progress tracking
+    }
+}

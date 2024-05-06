11cf15ec89888d2b0cf3e27e0239d7e9ef5ba7df
Shaoxiong Yuan <syuan697@usc.edu>
2024-05-05 23:05:58 -0700

    update

--- Dough to Bread/Dough to Bread/FinancialDashboardView.swift
+++ Dough to Bread/Dough to Bread/FinancialDashboardView.swift
@@ -1,57 +1,42 @@
-////
-////  FinancialDashboardView.swift
-////  Dough to Bread
-////
-////  Created by Shaoxiong Yuan on 3/27/24.
-////
-//
-//import SwiftUI
-//
-//struct FinancialDashboardView: View {
-//    // For simplicity, these are just example links and titles
-//    let videoLinks = [
-//        ("Financial Literacy - Video 1", "https://www.example.com/video1"),
-//        ("Financial Literacy - Video 2", "https://www.example.com/video2")
-//        // Add more links as needed
-//    ]
-//    
-//    let textLinks = [
-//        ("Understanding Your Taxes - Article", "https://www.example.com/article1"),
-//        ("Saving for Retirement - Article", "https://www.example.com/article2")
-//        // Add more links as needed
-//    ]
-//
-//    var body: some View {
-//        NavigationView {
-//            List {
-//                Section(header: Text("QuickBooks Integration")) {
-//                    // This is a placeholder for the QuickBooks integration.
-//                    Text("QuickBooks Data")
-//                        .onTapGesture {
-//                            // Implement QuickBooks authentication and data retrieval
-//                        }
-//                }
-//                
-//                Section(header: Text("Financial Literacy Videos")) {
-//                    ForEach(videoLinks, id: \.1) { link in
-//                        Link(link.0, destination: URL(string: link.1)!)
-//                    }
-//                }
-//                
-//                Section(header: Text("Financial Literacy Texts")) {
-//                    ForEach(textLinks, id: \.1) { link in
-//                        Link(link.0, destination: URL(string: link.1)!)
-//                    }
-//                }
-//            }
-//            .navigationBarTitle("Financial Dashboard")
-//        }
-//    }
-//}
-//
-//struct FinancialDashboardView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        FinancialDashboardView()
-//    }
-//}
-//
+
+import SwiftUI
+
+struct FinancialDashboardView: View {
+    let videoLinks = [
+        ("Financial Literacy - Video 1", "https://www.youtube.com/watch?v=4j2emMn7UaI"),
+        ("Financial Literacy - Video 2", "https://www.youtube.com/watch?v=0iRbD5rM5qc")
+        // Add more links as needed
+    ]
+
+    let textLinks = [
+        ("Why Financial Literacy Is So Important?", "https://www.forbes.com/sites/truetamplin/2023/09/21/financial-literacy--meaning-components-benefits--strategies/?sh=753cbad868cd"),
+        ("From Saving to Paying Debts", "https://www.cnbc.com/2022/04/01/why-financial-literacy-is-so-important.html")
+        // Add more links as needed
+    ]
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Financial Literacy Videos")) {
+                    ForEach(videoLinks, id: \.1) { link in
+                        Link(link.0, destination: URL(string: link.1)!)
+                    }
+                }
+
+                Section(header: Text("Financial Literacy Texts")) {
+                    ForEach(textLinks, id: \.1) { link in
+                        Link(link.0, destination: URL(string: link.1)!)
+                    }
+                }
+            }
+            .navigationBarTitle("Financial Dashboard")
+        }
+    }
+}
+
+
+
+#Preview {
+    FinancialDashboardView()
+}
+
